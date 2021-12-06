bfff7dbbb1a622e78e0859d61cbbfddb2fb3fa43
Ilya Chachua <cacuaila@gmail.com>
2021-12-06 09:59:58 +0200

    123

--- 123.js
+++ 123.js
@@ -1,16 +1,10 @@
-// const inputOdd = prompt('Вы хотите продлить подписку');
-// let message;
-// if (inputOdd === null || inputOdd.trim() === '') {
-//   alert('Invalid value');
-// } else {
-//   if (inputOdd.length % 2 === 0) {
-//     message = inputOdd.slice(inputOdd.length / 2 - 1, inputOdd.length / 2 + 1);
-//     alert(message);
-//   } else {
-//     message = inputOdd.slice(
-//       (inputOdd.length - 1) / 2,
-//       (inputOdd.length - 1) / 2 + 1,
-//     );
-//     alert(message);
-//   }
-// }
+const str = 'подотряд класса пресмыкающихся отряда чешуйчатые1';
+let res = '';
+for (let i = 0; i < str.length; i++) {
+  //console.log(res, i % 2);
+  if (i % 2) {
+    res += '*';
+  } else {
+    res += str[i];
+  }
+}
